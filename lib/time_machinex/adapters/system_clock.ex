d6d791a187ae1e3c5101f3a4eaaf9ee76fc58a1d
Ian Luites <IanLuites@users.noreply.github.com>
2019-12-16 17:37:24 +0100

    Add quoteed [utc_]now to SystemClock.

--- lib/time_machinex/adapters/system_clock.ex
+++ lib/time_machinex/adapters/system_clock.ex
@@ -10,4 +10,14 @@ defmodule TimeMachinex.SystemClock do
 
   @impl TimeMachinex.Adapter
   def utc_now, do: UTCDateTime.utc_now()
+
+  @impl TimeMachinex.Adapter
+  def quoted_now do
+    quote do: DateTime.utc_now()
+  end
+
+  @impl TimeMachinex.Adapter
+  def quoted_utc_now do
+    quote do: UTCDateTime.utc_now()
+  end
 end
